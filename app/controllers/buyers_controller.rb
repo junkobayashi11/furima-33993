66bf5e5d0bbd84690c7d93c907ca4158ca360fbb
junkobayashi11 <77084304+junkobayashi11@users.noreply.github.com>
2021-02-16 15:09:49 +0900

    テーブル・ルーティングの設定

---
+++ app/controllers/buyers_controller.rb
@@ -0,0 +1,44 @@
+class BuyersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_buyer, only: [:index, :create, :set_tramsition]
+  before_action :set_transition, only: :index
+
+  def index
+    @street_buyer = StreetBuyer.new
+  end
+
+  def create
+    @street_buyer = StreetBuyer.new(buyer_params)
+    if @street_buyer.valid?
+      pay_item
+      @street_buyer.save
+      return redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def buyer_params
+    params.require(:street_buyer).permit(:post_code, :municipality, :address, :building_name, :phone_number, :prefecture_id).merge(user_id: current_user.id, item_id: params[:item_id],token: params[:token])
+  end
+
+  def set_transition
+    if current_user.id == @buyer_street.user_id
+      redirect_to root_path
+    end
+  end
+
+  def set_buyer
+    @buyer_street = Item.find(params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+        amount: @buyer_street.price,
+        card: buyer_params[:token],
+        currency: 'jpy'
+    )
+  end
+end
---
+++ db/migrate/20210212031423_create_street_addresses.rb
@@ -0,0 +1,14 @@
+class CreateStreetAddresses < ActiveRecord::Migration[6.0]
+  def change
+    create_table :street_addresses do |t|
+      t.string     :post_code,     null: false
+      t.string     :municipality,  null: false
+      t.string     :address,       null: false
+      t.string     :building_name
+      t.string     :phone_number,  null: false
+      t.integer    :prefecture_id, null: false
+      t.references :buyer,         null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
